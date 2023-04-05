f19d36b753943f02d5cdab484f81b986552e5a5a
Jameel <abdul.jameel@siemens-healthineers.com>
2023-04-05 17:26:33 +0530

    added support for chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,11 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-// import Message from "./Message";
-// import ListGroup from "./components/ListGroup/ListGroup";
-// import Alerts from "./components/Alerts";
-// import Buttons from "./components/Buttons";
-// import ExpandableText from "./components/ExpandableText";
-// import Form from "./components/Form";
-// import ExpenseList from "./expense-tracker/components/ExpenseList";
-// import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-// import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-// import categories from "./expense-tracker/Categories";
 import axios from "axios";
-import { useEffect, useState } from "react";
-interface User {
-  id: number;
-  name: string;
-}
+import { Button, ButtonGroup } from '@chakra-ui/react'
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      var response = await axios.get<User[]>(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      console.log(response.data);
-      setUsers(response.data);
-    }
-    fetchData();
-  }, []);
-
-  return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-  );
+  return(
+    <div>
+      <Button colorScheme="blue">Hello</Button>
+    </div>
+  )
 }
 
 export default App;
