73957472af29edc2307d054d20b785a6a47e82c0
Jameel <abdul.jameel@siemens-healthineers.com>
2023-09-09 20:40:33 +0530

    game screen shots

--- src/game-hub/hooks/useScreenshots.tsx
+++ src/game-hub/hooks/useScreenshots.tsx
@@ -3,13 +3,14 @@ import ApiClient from "../services/api-client";
 import { Game } from "../entities/Game";
 import { Trailer } from "../entities/Trailer";
 import { AxiosRequestConfig } from "axios";
+import { Screenshot } from "../entities/Screenshot";
 
-const useGameTrailer = (gameId: number) => {
-  const apiClient = new ApiClient<Trailer>(`/games/${gameId}/movies`);
+const useScreenshots = (gameId: number) => {
+  const apiClient = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`);
   return useQuery({
-    queryKey: ["gameMovies", gameId],
+    queryKey: ["screenshots", gameId],
     queryFn: () => apiClient.getAll({} as AxiosRequestConfig),
   });
 };
 
-export default useGameTrailer;
+export default useScreenshots;
